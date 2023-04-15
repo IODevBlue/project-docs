93dc8d10163ecad44aa3c8c372c1cb238acbed7e
IODevBlue <iodevblue@gmail.com>
2023-04-15 20:22:49 +0100

    BannerX documentations

---
+++ api/android/bannerx/html/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":bannerx:dokkaHtml/androidTestRelease":[],":bannerx:dokkaHtml/debug":[],":bannerx:dokkaHtml/main":[],":bannerx:dokkaHtml/release":[],":bannerx:dokkaHtml/testFixtures":[],":bannerx:dokkaHtml/testFixturesDebug":[],":bannerx:dokkaHtml/testFixturesRelease":[]}'
---
+++ api/android/bannerx/javadoc/module-search-index.js
@@ -0,0 +1,1 @@
+var moduleSearchIndex = [{"l":"BannerX","url":"index.html"}]
